bf9be77e0daf54d98437ff62e5882a60a61c3032
aaron.chen <aaron.chen@perficient.com>
2017-10-10 17:04:34 +0800

    removed ARToolKit

--- app/src/main/java/com/perficient/meetingschedulear/BaseApplication.java
+++ app/src/main/java/com/perficient/meetingschedulear/BaseApplication.java
@@ -3,7 +3,6 @@ package com.perficient.meetingschedulear;
 import android.app.Application;
 import android.content.Context;
 
-import org.artoolkit.ar6.base.assets.AssetHelper;
 
 public class BaseApplication extends Application {
 
@@ -17,15 +16,5 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getContext();
-        this.initializeInstance();
-    }
-
-    // Here we do one-off initialisation which should apply to all activities
-    // in the application.
-    protected void initializeInstance() {
-
-        AssetHelper assetHelper = new AssetHelper(getAssets());
-        assetHelper.cacheAssetFolder(this, "Data");
-        assetHelper.cacheAssetFolder(this, "cparam_cache");
     }
 }
